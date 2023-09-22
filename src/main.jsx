1898bef11014cdd5f161ec58c69f0efa5bc62520
srs-sieam <srssieam@gmail.com>
2023-09-22 06:41:14 +0600

    created Root.jsx component and set outlet

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,12 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import './index.css'
+import Root from './component/Root/Root';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root></Root>,
   },
 ]);
 
