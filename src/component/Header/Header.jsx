0723dc2c01da7ee10b51edf7d155f6ebada2e400
srs-sieam <srssieam@gmail.com>
2023-09-22 08:10:47 +0600

    customized header and footer style

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,5 +1,12 @@
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
+    const links = <>
+        <li><NavLink to ='/'>Home</NavLink></li>
+        <li><NavLink to='statistics'>Statistics</NavLink></li>
+        <li><NavLink to='applied-jobs'>Applied Jobs</NavLink></li>
+        <li><NavLink to='blog'>Blog</NavLink></li>
+    </>
     return (
         <div>
             <div className="navbar bg-base-100">
@@ -9,36 +16,18 @@ const Header = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
                         <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><a>Item 1</a></li>
-                            <li>
-                                <a>Parent</a>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </li>
-                            <li><a>Item 3</a></li>
+                            {links}
                         </ul>
                     </div>
-                    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                    <a className="text-transparent bg-clip-text bg-gradient-to-r to-purple-600 from-blue-500 normal-case md:text-3xl font-bold">Career Hub</a>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
-                        <li><a>Item 1</a></li>
-                        <li tabIndex={0}>
-                            <details>
-                                <summary>Parent</summary>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </details>
-                        </li>
-                        <li><a>Item 3</a></li>
+                        {links}
                     </ul>
                 </div>
                 <div className="navbar-end">
-                    <a className="btn">Button</a>
+                    <a className="btn bg-gradient-to-r to-violet-500 from-indigo-500 font-bold text-white">Star Applying</a>
                 </div>
             </div>
         </div>
