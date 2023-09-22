14b1cc0918ca2fc27e11eac5f89462258cf56691
srs-sieam <srssieam@gmail.com>
2023-09-22 18:51:50 +0600

    used toast and store applied job to localStorage

--- src/component/Featured Jobs/FeaturedJobs.jsx
+++ src/component/Featured Jobs/FeaturedJobs.jsx
@@ -16,7 +16,7 @@ const FeaturedJobs = () => {
     return (
         <div>
             <div className="text-center my-12">
-                <h1 className="text-xl md:text-3xl lg:text-5xl font-bold">Featured Jobs {jobs.length}</h1>
+                <h1 className="text-xl md:text-3xl lg:text-5xl font-bold">Featured Jobs</h1>
                 <p className="mt-5">Explore thousands of job opportunities with all the information you need. Its your future</p>
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-4">
--- src/component/JobDetails/JobDetails.jsx
+++ src/component/JobDetails/JobDetails.jsx
@@ -2,6 +2,9 @@ import { useLoaderData, useParams } from "react-router-dom";
 import { HiOutlineCurrencyDollar } from 'react-icons/Hi';
 import { MdOutlineDateRange, MdOutlineMail } from 'react-icons/Md';
 import { BsTelephone } from 'react-icons/Bs';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { saveJobApplication } from "../Utility/LocalStorage";
 
 
 const JobDetails = () => {
@@ -9,7 +12,13 @@ const JobDetails = () => {
     const { id } = useParams();  // Here, useParams() extracts the id parameter from the URL
     const job = jobs.find(job => job.id === parseInt(id));  // we converted id from string to number
     console.log(job, id)
-    const { job_description, job_responsibility, educational_requirements, experiences, salary, job_title, contact_information} = job
+    const { job_description, job_responsibility, educational_requirements, experiences, salary, job_title, contact_information } = job;
+
+    const notify = () =>{
+        saveJobApplication(id)
+        toast("Applied successfully");
+    } 
+
     return (
         <div className="md:my-8 lg:my-32">
             <h2 className="text-3xl font-bold text-center">Job Details</h2>
@@ -17,7 +26,7 @@ const JobDetails = () => {
                 <div className="md:col-span-2 space-y-11 text-sm">
                     <p><strong>Job Description:</strong> {job_description}</p>
                     <p><strong>Job Responsibility:</strong> {job_responsibility}</p>
-                    <p><strong>Educational Requirements:</strong><br/>{educational_requirements}</p>
+                    <p><strong>Educational Requirements:</strong><br />{educational_requirements}</p>
                     <p><strong>Experiences:</strong><br /> {experiences}</p>
                 </div>
                 <div className="col-span-1 bg-slate-100 rounded-xl space-y-6 p-2">
@@ -30,7 +39,19 @@ const JobDetails = () => {
                     <p className="text-xs lg:text-sm flex items-center gap-2"><BsTelephone className='text-xl text-indigo-600' /><strong>Phone :</strong> {contact_information.phone}</p>
                     <p className="text-xs lg:text-sm flex items-center gap-2"><MdOutlineMail className='text-xl text-indigo-600' /><strong>Email :</strong> {contact_information.email}</p>
                     <p className="text-xs lg:text-sm flex items-center gap-2"><MdOutlineMail className='text-xl text-indigo-600' /><strong>Address :</strong> {contact_information.address}</p>
-                    <button className="btn bg-gradient-to-r to-violet-500 from-indigo-500 font-semibold text-white normal-case w-full">Apply Now</button>
+                    <button onClick={notify} className="btn bg-gradient-to-r to-violet-500 from-indigo-500 font-semibold text-white normal-case w-full">Apply Now</button>
+                    <ToastContainer
+                        position="top-center"
+                        autoClose={5000}
+                        hideProgressBar={false}
+                        newestOnTop={false}
+                        closeOnClick
+                        rtl={false}
+                        pauseOnFocusLoss
+                        draggable
+                        pauseOnHover
+                        theme="light"
+                    />
                 </div>
             </div>
         </div>
